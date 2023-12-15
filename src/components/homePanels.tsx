92b963010894174fee7495b44812cfb4fa9543f2
juliscapucin <jscapucin@gmail.com>
2023-12-15 15:05:48 +0100

    home: add homePanels links functionality

--- src/components/homePanels.tsx
+++ src/components/homePanels.tsx
@@ -9,34 +9,36 @@ import { ScrollTrigger } from "gsap/ScrollTrigger"
 export default function homePanels() {
 	const outerContainerRef = useRef<HTMLDivElement | null>(null)
 	const panelsContainerRef = useRef<HTMLDivElement | null>(null)
+	const headerRef = useRef<HTMLDivElement | null>(null)
 	let tween: gsap.core.Tween
 
 	/* Main navigation */
-	const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+	const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
 		const container = panelsContainerRef.current
 		if (!tween.scrollTrigger || !container) return
-		e.preventDefault()
 
-		const targetElem = e.target as HTMLAnchorElement
-		const targetAttr = targetElem.getAttribute("href")
+		const targetElem = e.target as HTMLButtonElement
+		const targetAttr = targetElem.getAttribute("data-href")
 
-		const targetPanel = document.querySelector(targetAttr!) as HTMLDivElement
+		const targetPanel = document.querySelector(
+			`[data-id=${targetAttr}]`
+		) as HTMLDivElement
+		let y = targetPanel!.offsetLeft
 
-		let y = targetPanel!.offsetTop
+		// TO DO: check if this is necessary
+		// if (
+		// 	targetAttr &&
+		// 	panelsContainerRef.current!.isSameNode(targetPanel!.parentElement)
+		// ) {
+		// 	let totalScroll = tween.scrollTrigger.end - tween.scrollTrigger.start,
+		// 		totalMovement = container.scrollWidth - window.innerWidth
+		// 	y = Math.round(
+		// 		tween.scrollTrigger.start +
+		// 			(targetElem.offsetLeft / totalMovement) * totalScroll
+		// 	)
+		// }
 
-		if (
-			targetAttr &&
-			panelsContainerRef.current!.isSameNode(targetPanel!.parentElement)
-		) {
-			let totalScroll = tween.scrollTrigger.end - tween.scrollTrigger.start,
-				totalMovement = container.scrollWidth - innerWidth
-			y = Math.round(
-				tween.scrollTrigger.start +
-					(targetElem.offsetLeft / totalMovement) * totalScroll
-			)
-		}
-
-		// console.log("useful stuff!", y, e.target.getAttribute("href"))
+		// console.log("useful stuff!", y, targetAttr)
 
 		gsap.to(window, {
 			scrollTo: {
@@ -55,233 +57,98 @@ export default function homePanels() {
 		const container = panelsContainerRef.current
 		const panels = gsap.utils.toArray(".panel")
 
-		tween = gsap.to(panels, {
-			x: () => -1 * (container.scrollWidth - innerWidth),
-			ease: "none",
-			scrollTrigger: {
-				trigger: container,
-				pin: true,
-				start: "top top",
-				scrub: 1,
-				end: () => "+=" + (container.scrollWidth - innerWidth),
-				onUpdate: (self) => {
-					// also useful!
-					// console.log(self.progress, '/1')
-					// console.log(window.scrollY, `/${document.body.scrollHeight - window.innerHeight}`)
+		let ctx = gsap.context(() => {
+			tween = gsap.to(panels, {
+				x: () => -1 * (container.scrollWidth - window.innerWidth),
+				ease: "none",
+				scrollTrigger: {
+					trigger: outerContainerRef.current,
+					pin: true,
+					start: "top top",
+					scrub: 1,
+					end: () => "+=" + (container.scrollWidth - window.innerWidth),
+					// onUpdate: (self) => {
+					// 	console.log(self.progress, "/1")
+					// 	console.log(
+					// 		window.scrollY,
+					// 		`/${document.body.scrollHeight - window.innerHeight}`
+					// 	)
+					// },
 				},
-			},
-		})
+			})
+		}, container)
+
+		return () => {
+			ctx.revert()
+		}
 	}, [outerContainerRef, panelsContainerRef])
 
 	return (
 		<div id='page' className='site'>
 			<div id='feather' className='feather'></div>
 
-			<header id='masthead' className='site-header' role='banner'>
-				<nav className='anchor-nav' role='navigation'>
-					<button data-href='panel-1' className='anchor'>
+			<header ref={headerRef} className='fixed z-100'>
+				<nav className='flex gap-8'>
+					<button data-href='panel-1' onClick={(e) => handleClick(e)}>
 						Panel 1
 					</button>
-					<button data-href='panel-2' className='anchor'>
+					<button data-href='panel-2' onClick={(e) => handleClick(e)}>
 						Panel 2
 					</button>
-					<button data-href='panel-3' className='anchor'>
+					<button data-href='panel-3' onClick={(e) => handleClick(e)}>
 						Panel 3
 					</button>
-					<button data-href='panel-4' className='anchor'>
+					<button data-href='panel-4' onClick={(e) => handleClick(e)}>
 						Panel 4
-					</button>
-					<button data-href='panel-5' className='anchor'>
-						Panel 5
-					</button>
-					<button data-href='map' className='anchor'>
-						Map
 					</button>
 				</nav>
 			</header>
 
-			<main id='content' className='site-content' role='main'>
-				<section ref={outerContainerRef} id='panels'>
-					<div ref={panelsContainerRef} id='panels-container' className='flex'>
-						<article id='panel-1' className='panel full-screen red'>
-							<div className='container'>
-								<div className='row'>
-									<div className='col-6'>
-										<h1 className='text-displayLarge'>Panel</h1>
-									</div>
-									<div className='col-6 d-flex flex-column'>
-										<h2>Panel 1</h2>
+			<section ref={outerContainerRef} id='panels'>
+				<div ref={panelsContainerRef} id='panels-container' className='flex'>
+					{Array.from({ length: 4 }).map((_, index) => (
+						<article
+							data-id={`panel-${index + 1}`}
+							className='panel min-w-full'
+							key={`panel-${index}`}
+						>
+							<div className='container mt-32'>
+								<div className='col-6'>
+									<h1 className='text-displayLarge'>Panel {index + 1}</h1>
+								</div>
+								<div className='col-6 d-flex flex-column'>
+									<p className='step-description'>
+										Lorem Ipsum is simply dummy text of the printing and
+										typesetting industry. Including versions of Lorem Ipsum.
+									</p>
 
-										<p className='step-description'>
-											Lorem Ipsum is simply dummy text of the printing and
-											typesetting industry. Including versions of Lorem Ipsum.
-										</p>
-
-										<div className='panels-navigation text-right'>
-											<div className='nav-panel' data-sign='plus'>
-												<a href='#panel-2' className='anchor'>
-													Next
-												</a>
-											</div>
+									<div className='panels-navigation'>
+										<div className='nav-panel' data-sign='minus'>
+											<button
+												data-href={
+													index - 1 > 1 ? `panel-${index - 1}` : "panel-1"
+												}
+												onClick={(e) => handleClick(e)}
+											>
+												Prev
+											</button>
+										</div>
+										<div className='nav-panel' data-sign='plus'>
+											<button
+												data-href={
+													index + 1 < 4 ? `panel-${index + 1}` : "panel-4"
+												}
+											>
+												Next
+											</button>
 										</div>
 									</div>
 								</div>
 							</div>
 						</article>
-						<article id='panel-2' className='panel full-screen orange'>
-							<div className='container'>
-								<div className='row'>
-									<div className='col-6'>
-										<h1 className='text-displayLarge'>Panel</h1>
-									</div>
-									<div className='col-6 d-flex flex-column'>
-										<h2>Panel 2</h2>
-
-										<p className='step-description'>
-											Lorem Ipsum is simply dummy text of the printing and
-											typesetting industry. Including versions of Lorem Ipsum.
-										</p>
-
-										<div className='panels-navigation'>
-											<div className='nav-panel' data-sign='minus'>
-												<a href='#panel-1' className='anchor'>
-													Prev
-												</a>
-											</div>
-											<div className='nav-panel' data-sign='plus'>
-												<a href='#panel-3' className='anchor'>
-													Next
-												</a>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</article>
-						<article id='panel-3' className='panel full-screen purple'>
-							<div className='container'>
-								<div className='row'>
-									<div className='col-6'>
-										<h1 className='text-displayLarge'>Panel</h1>
-									</div>
-									<div className='col-6 d-flex flex-column'>
-										<h2>Panel 3</h2>
-
-										<p className='step-description'>
-											Lorem Ipsum is simply dummy text of the printing and
-											typesetting industry. Including versions of Lorem Ipsum.
-										</p>
-
-										<div className='panels-navigation'>
-											<div className='nav-panel' data-sign='minus'>
-												<a href='#panel-2' className='anchor'>
-													Prev
-												</a>
-											</div>
-											<div className='nav-panel' data-sign='plus'>
-												<a href='#panel-4' className='anchor'>
-													Next
-												</a>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</article>
-						<article id='panel-4' className='panel full-screen green'>
-							<div className='container'>
-								<div className='row'>
-									<div className='col-6'>
-										<h1 className='text-displayLarge'>Panel</h1>
-									</div>
-									<div className='col-6 d-flex flex-column'>
-										<h2>Panel 4</h2>
-
-										<p className='step-description'>
-											Lorem Ipsum is simply dummy text of the printing and
-											typesetting industry. Including versions of Lorem Ipsum.
-										</p>
-										<div className='card'>test</div>
-										<div className='card'>test</div>
-										<div className='card'>test</div>
-										<div className='card'>test</div>
-										<div className='card'>test</div>
-										<div className='card'>test</div>
-										<div className='card'>test</div>
-										<div className='card'>test</div>
-										<div className='card'>test</div>
-										<div className='card'>test</div>
-
-										<div className='panels-navigation'>
-											<div className='nav-panel' data-sign='minus'>
-												<a href='#panel-3' className='anchor'>
-													Prev
-												</a>
-											</div>
-											<div className='nav-panel' data-sign='plus'>
-												<a href='#panel-5' className='anchor'>
-													Next
-												</a>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</article>
-						<article id='panel-5' className='panel full-screen gray'>
-							<div className='container'>
-								<div className='row'>
-									<div className='col-6'>
-										<h1 className='text-displayLarge'>Panel</h1>
-									</div>
-									<div className='col-6 d-flex flex-column'>
-										<h2>Panel 5</h2>
-
-										<p className='step-description'>
-											Lorem Ipsum is simply dummy text of the printing and
-											typesetting industry. Including versions of Lorem Ipsum.
-										</p>
-										<div className='cards-wrapper'>
-											<div className='card'>test</div>
-											<div className='card'>test</div>
-											<div className='card'>test</div>
-											<div className='card'>test</div>
-											<div className='card'>test</div>
-											<div className='card'>test</div>
-											<div className='card'>test</div>
-											<div className='card'>test</div>
-											<div className='card'>test</div>
-											<div className='card'>test</div>
-											<div className='card'>test</div>
-
-											<div className='card'>test</div>
-											<div className='card'>test</div>
-											<div className='card'>test</div>
-											<div className='card'>test</div>
-											<div className='card'>test</div>
-											<div className='card'>test</div>
-											<div className='card'>test</div>
-											<div className='card'>test</div>
-											<div className='card'>test</div>
-											<div className='card'>test</div>
-										</div>
-
-										<div className='panels-navigation text-right'>
-											<div className='nav-panel' data-sign='minus'>
-												<a href='#panel-4' className='anchor'>
-													Prev
-												</a>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</article>
-					</div>
-				</section>
-
-				<section id='map' className='full-screen'></section>
-			</main>
+					))}
+				</div>
+			</section>
 		</div>
 	)
 }
