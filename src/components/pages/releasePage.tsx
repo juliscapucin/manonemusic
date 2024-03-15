ee649244e93444e058c73c61ea7d028070f8d9dd
juliscapucin <jscapucin@gmail.com>
2024-03-15 14:33:11 +0100

    releases: improve releasePage back transition

--- src/components/pages/releasePage.tsx
+++ src/components/pages/releasePage.tsx
@@ -40,17 +40,24 @@ export default function ReleasePage({
 			gsap.to(".gsap-releases-page", { xPercent: 0, duration: 0.3 })
 			gsap.set(".gsap-release-image", { opacity: 0 })
 
-			gsap.to(".gsap-release-content", { opacity: 0, duration: 0.3 })
-
-			Flip.fit(".gsap-flip-release-card", stateCard, {
-				scale: true,
-				absolute: true,
-				duration: 0.6,
-				ease: "power4.out",
+			gsap.to(".gsap-release-content", {
+				opacity: 0,
+				duration: 0.3,
 				onComplete: () => {
 					router.push(slug)
 				},
 			})
+
+			// TODO - delete if unused
+			// Flip.fit(".gsap-flip-release-card", stateCard, {
+			// 	scale: true,
+			// 	absolute: true,
+			// 	duration: 0.6,
+			// 	ease: "power4.out",
+			// 	onComplete: () => {
+			// 		router.push(slug)
+			// 	},
+			// })
 		})
 	}
 
@@ -126,10 +133,13 @@ export default function ReleasePage({
 				className='gsap-release-page w-3/4 h-screen p-8 pt-32 ml-auto'
 			>
 				{/* Back Button */}
-				<Button action={() => transitionOnClickBack("/releases")}>
+				<Button
+					classes='absolute'
+					action={() => transitionOnClickBack("/releases")}
+				>
 					Back to releases
 				</Button>
-				<TitleHeadline>{releaseData.title}</TitleHeadline>
+				<TitleHeadline classes='mt-6'>{releaseData.title}</TitleHeadline>
 				<div className='relative w-full flex gap-8'>
 					<div className='gsap-release-page flex flex-wrap gap-16'>
 						<div className='gsap-flip-release-image relative w-1/4 min-w-[300px] aspect-square overflow-clip'>
