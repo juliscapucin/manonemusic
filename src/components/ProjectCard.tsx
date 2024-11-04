bd49dca503f5414fd61a7fb7df06294357ea24b5
juliscapucin <jscapucin@gmail.com>
2024-11-04 10:07:22 +0100

    releases: add hover to project card

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -34,15 +34,15 @@ export default function ProjectCard({
 					)
 				}}
 				href={`/${section}/${slug}`}
-				classes='gsap-project-card w-1/6 relative'
+				classes='gsap-project-card w-1/6 relative group'
 				aria-labelledby={`project-title-${slug}`}
 			>
 				{image && (
-					<div className='relative w-full aspect-square overflow-clip'>
+					<div className='relative w-full aspect-square overflow-clip z-5'>
 						<Image
 							className={`${
 								pathname.includes(slug) && "gsap-project-card z-100"
-							} relative w-1/6 aspect-square overflow-clip object-cover`}
+							} relative w-1/6 aspect-square overflow-clip object-cover group-hover:scale-105 transition-transform duration-300`}
 							src={image.imageUrl}
 							alt={image.imageAlt}
 							sizes='50vw'
@@ -51,7 +51,7 @@ export default function ProjectCard({
 					</div>
 				)}
 				<span
-					className='text-labelMedium lg:text-labelLarge uppercase text-left leading-none'
+					className='block text-labelMedium lg:text-labelLarge uppercase text-left leading-none opacity-0 -translate-y-full group-hover:opacity-100 group-hover:translate-y-4 transition-all duration-300'
 					id={`project-title-${slug}`}
 				>
 					{title}
