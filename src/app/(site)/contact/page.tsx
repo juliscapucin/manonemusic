38d256bb2a78043bf2fbf7e9495f0b31296f9a38
juliscapucin <jscapucin@gmail.com>
2024-11-11 22:41:18 +0100

    hotfix: add force-no-store

--- src/app/(site)/contact/page.tsx
+++ src/app/(site)/contact/page.tsx
@@ -4,6 +4,7 @@ import { Panels } from "@/components/server"
 
 // Opt out of caching for all data requests in the route segment
 export const dynamic = "force-dynamic"
+export const fetchCache = "force-no-store"
 
 export const metadata: Metadata = {
 	title: "Man One Music",
--- src/components/server/panels.tsx
+++ src/components/server/panels.tsx
@@ -11,6 +11,7 @@ import {
 
 // Opt out of caching for all data requests in the route segment
 export const dynamic = "force-dynamic"
+export const fetchCache = "force-no-store"
 
 import { PanelDesktop, PanelMobile } from "@/components"
 
