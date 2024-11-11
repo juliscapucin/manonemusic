fe24bd84ebd6752312b99340a5505d1a943d4245
juliscapucin <jscapucin@gmail.com>
2024-11-11 09:58:12 +0100

    pages: delete unused page components

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,7 +8,8 @@ export const colors = {
 	colorWhite: "#B6D1C2",
 }
 
-export const ease = [0.6, 0.05, -0.01, 0.9]
+// export const ease = [0.6, 0.05, -0.01, 0.9]
+export const customEase = "1, 0.01, 0.01, 1"
 
 export const navLinks = [
 	{ label: "Home", slug: "/" },
