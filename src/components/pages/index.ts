73461d8a877223ad7608fdf01bbbb159b009bf93
juliscapucin <jscapucin@gmail.com>
2024-11-11 10:00:43 +0100

    pages: remove imports

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -1,11 +1,8 @@
 import AboutPage from "./aboutPage"
-import CommercialPage from "./commercialPage"
 import CommercialsPage from "./commercialsPage"
 import ContactPage from "./contactPage"
-import FilmPage from "./filmPage"
 import FilmsPage from "./filmsPage"
 import HomePage from "./homePage"
-import ReleasePage from "./releasePage"
 import ReleasesPage from "./releasesPage"
 import ProjectsPage from "./projectsPage"
 import ProjectPage from "./projectPage"
@@ -13,12 +10,9 @@ import ProjectPage from "./projectPage"
 export {
 	AboutPage,
 	ContactPage,
-	CommercialPage,
 	CommercialsPage,
-	FilmPage,
 	FilmsPage,
 	HomePage,
-	ReleasePage,
 	ReleasesPage,
 	ProjectPage,
 	ProjectsPage,
