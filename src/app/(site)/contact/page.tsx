98d57a71e7aa948674236801e0fd21014a323672
juliscapucin <jscapucin@gmail.com>
2024-11-11 22:10:30 +0100

    hotfix: add force-dynamic to pages

--- src/app/(site)/contact/page.tsx
+++ src/app/(site)/contact/page.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next"
 
 import { Panels } from "@/components/server"
 
+// Opt out of caching for all data requests in the route segment
+export const dynamic = "force-dynamic"
+
 export const metadata: Metadata = {
 	title: "Man One Music",
 	description:
--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,5 +1,8 @@
 import { Panels } from "@/components/server"
 
+// Opt out of caching for all data requests in the route segment
+export const dynamic = "force-dynamic"
+
 export default async function Home() {
 	return (
 		<main className='w-full lg:h-full'>
