ffd739522c301c3e701d077f63c0fe7b52852b2d
juliscapucin <jscapucin@gmail.com>
2025-06-02 11:09:20 +0100

    fix home schema

--- src/sanity/schemas/home-page-schema.ts
+++ src/sanity/schemas/home-page-schema.ts
@@ -1,42 +1,37 @@
-import { Rule } from "sanity"
+import { Rule } from "sanity";
 
 const homePageSchema = {
-	name: "homePage",
-	title: "Home Page",
-	type: "document",
-	fields: [
-		{
-			name: "title",
-			title: "Title (required)",
-			type: "string",
-			validation: (Rule: Rule) => Rule.required().error("Title is required"),
-		},
-		{
-			name: "subtitle",
-			title: "Subtitle (required)",
-			type: "string",
-			validation: (Rule: Rule) => Rule.required().error("Subtitle is required"),
-		},
-		{
-			name: "content",
-			title: "Content (required)",
-			type: "array",
-			of: [{ type: "block" }],
-			validation: (Rule: Rule) => Rule.required().error("Content is required"),
-		},
-		{ name: "metadataTitle", title: "Metadata Title", type: "string" },
-		{
-			name: "metadataDescription",
-			title: "Metadata Description",
-			type: "string",
-		},
-		{
-			name: "metadataKeywords",
-			title: "Metadata Keywords",
-			type: "array",
-			of: [{ type: "string" }],
-		},
-	],
-}
+   name: "homePage",
+   title: "Home Page",
+   type: "document",
+   fields: [
+      {
+         name: "title",
+         title: "Title (required)",
+         type: "string",
+         validation: (Rule: Rule) => Rule.required().error("Title is required"),
+      },
+      {
+         name: "subtitle",
+         title: "Subtitle (required)",
+         type: "array",
+         of: [{ type: "block" }],
+         validation: (Rule: Rule) =>
+            Rule.required().error("Subtitle is required"),
+      },
+      { name: "metadataTitle", title: "Metadata Title", type: "string" },
+      {
+         name: "metadataDescription",
+         title: "Metadata Description",
+         type: "string",
+      },
+      {
+         name: "metadataKeywords",
+         title: "Metadata Keywords",
+         type: "array",
+         of: [{ type: "string" }],
+      },
+   ],
+};
 
-export default homePageSchema
+export default homePageSchema;
