8e605805a6120ee7151af1d25b914cb0712582fa
juliscapucin <jscapucin@gmail.com>
2025-06-02 10:45:44 +0100

    fix about schema

--- src/components/panelMobile.tsx
+++ src/components/panelMobile.tsx
@@ -1,52 +1,53 @@
-"use client"
+"use client";
 
-import gsap from "gsap"
-import { ScrollToPlugin } from "gsap/ScrollToPlugin"
+import gsap from "gsap";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
-import { AllData } from "@/types"
-import { usePathname } from "next/navigation"
-import { PanelContent } from "@/components"
-import { useLayoutEffect } from "react"
+import { AllData } from "@/types";
+import { usePathname } from "next/navigation";
+import { PanelContent } from "@/components";
+import { useLayoutEffect } from "react";
 
 export default function PanelMobile({ data }: { data: AllData }) {
-	const pathname = usePathname()
+   const pathname = usePathname();
 
-	const fullHeightSections = ["/", "contact"]
+   const fullHeightSections = ["/", "contact"];
 
-	useLayoutEffect(() => {
-		gsap.registerPlugin(ScrollToPlugin)
+   useLayoutEffect(() => {
+      gsap.registerPlugin(ScrollToPlugin);
 
-		if (pathname === "/") {
-			gsap.to(window, {
-				scrollTo: {
-					y: "#panel-home",
-					offsetY: 0,
-				},
-				duration: 0.5,
-			})
-			return
-		}
+      if (pathname === "/") {
+         gsap.to(window, {
+            scrollTo: {
+               y: "#panel-home",
+               offsetY: 0,
+            },
+            duration: 0.5,
+         });
+         return;
+      }
 
-		gsap.to(window, {
-			scrollTo: {
-				y: `#panel-${pathname.split("/")[1]}`,
-				// offsetY: 20,
-			},
-			duration: 0.5,
-		})
-	}, [pathname])
+      gsap.to(window, {
+         scrollTo: {
+            y: `#panel-${pathname.split("/")[1]}`,
+            // offsetY: 20,
+         },
+         duration: 0.5,
+      });
+   }, [pathname]);
 
-	return (
-		<div className='landscape:hidden'>
-			{data.headerNavLinks.map((section, index) => (
-				<section
-					id={`panel-${section.slug === "/" ? "home" : section.slug}`}
-					data-id={`panel-${section.slug}`}
-					className={`panel w-screen overflow-x-clip ${fullHeightSections.includes(section.slug) ? "h-svh" : "h-auto"}`}
-					key={`panel-${section.slug}`}>
-					<PanelContent data={data} section={section.slug} index={index + 1} />
-				</section>
-			))}
-		</div>
-	)
+   return (
+      <div className="landscape:hidden">
+         {data.headerNavLinks.map((section, index) => (
+            <section
+               id={`panel-${section.slug === "/" ? "home" : section.slug}`}
+               data-id={`panel-${section.slug}`}
+               className={`panel w-screen overflow-x-clip ${fullHeightSections.includes(section.slug) ? "h-svh" : "h-auto"}`}
+               key={`panel-${section.slug}`}
+            >
+               <PanelContent data={data} section={section.slug} />
+            </section>
+         ))}
+      </div>
+   );
 }
--- src/sanity/schemas/about-page-schema.ts
+++ src/sanity/schemas/about-page-schema.ts
@@ -1,71 +1,71 @@
-import { Rule } from "sanity"
+import { Rule } from "sanity";
 
 const aboutPageSchema = {
-	name: "aboutPage",
-	title: "About Page",
-	type: "document",
-	fields: [
-		{
-			name: "title",
-			title: "Title (required)",
-			type: "string",
-			validation: (Rule: Rule) => Rule.required().error("Title is required"),
-		},
-		{
-			name: "subtitle",
-			title: "Subtitle",
-			type: "text",
-			rows: 3,
-		},
-		{
-			name: "image",
-			type: "object",
-			title: "Image (required)",
-			fields: [
-				{
-					name: "image",
-					title: "Image",
-					type: "image",
-					validation: (Rule: Rule) =>
-						Rule.required().error("Image file is required"),
-				},
-				{
-					name: "imageAlt",
-					title: "Image Alt Text",
-					type: "string",
-					validation: (Rule: Rule) =>
-						Rule.required().error("Image Alt Text is required"),
-				},
-			],
-			validation: (Rule: Rule) => Rule.required().error("Image is required"),
-		},
-		{
-			name: "content1",
-			title: "Content (required)",
-			type: "array",
-			of: [{ type: "block" }],
-			validation: (Rule: Rule) => Rule.required().error("Content is required"),
-		},
-		{
-			name: "content2",
-			title: "Content (required)",
-			type: "array",
-			of: [{ type: "block" }],
-			validation: (Rule: Rule) => Rule.required().error("Content is required"),
-		},
-		{ name: "metadataTitle", title: "Metadata Title", type: "string" },
-		{
-			name: "metadataDescription",
-			title: "Metadata Description",
-			type: "string",
-		},
-		{
-			name: "metadataKeywords",
-			title: "Metadata Keywords",
-			type: "array",
-			of: [{ type: "string" }],
-		},
-	],
-}
+   name: "aboutPage",
+   title: "About Page",
+   type: "document",
+   fields: [
+      {
+         name: "title",
+         title: "Title (required)",
+         type: "string",
+         validation: (Rule: Rule) => Rule.required().error("Title is required"),
+      },
+      {
+         name: "subtitle",
+         title: "Subtitle",
+         type: "text",
+         rows: 3,
+      },
+      {
+         name: "image",
+         type: "object",
+         title: "Image (required)",
+         fields: [
+            {
+               name: "image",
+               title: "Image",
+               type: "image",
+               validation: (Rule: Rule) =>
+                  Rule.required().error("Image file is required"),
+            },
+            {
+               name: "imageAlt",
+               title: "Image Alt Text",
+               type: "string",
+               validation: (Rule: Rule) =>
+                  Rule.required().error("Image Alt Text is required"),
+            },
+         ],
+         validation: (Rule: Rule) => Rule.required().error("Image is required"),
+      },
+      {
+         name: "content1",
+         title: "Content 1 (required)",
+         type: "array",
+         of: [{ type: "block" }],
+         validation: (Rule: Rule) =>
+            Rule.required().error("Content is required"),
+      },
+      {
+         name: "content2",
+         title: "Content 2",
+         type: "array",
+         of: [{ type: "block" }],
+      },
+      { name: "metadataTitle", title: "Metadata Title", type: "string" },
+      {
+         name: "metadataDescription",
+         title: "Metadata Description",
+         type: "string",
+      },
+      {
+         name: "metadataKeywords",
+         title: "Metadata Keywords",
+         type: "array",
+         of: [{ type: "string" }],
+      },
+   ],
+};
 
-export default aboutPageSchema
+export default aboutPageSchema;
