aa300268e52a9a68bdb2d01dbcbfea5d5584a11c
juliscapucin <jscapucin@gmail.com>
2025-09-12 16:36:52 +0100

    refactor status component

--- src/components/status.tsx
+++ src/components/status.tsx
@@ -1,97 +1,98 @@
-"use client";
+'use client';
 
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 type StatusProps = {
-   location: string;
+    location: string;
 };
 
 type Day = {
-   dayOfWeek: string;
-   dayOfMonth: string;
-   month: string;
+    dayOfWeek: string;
+    dayOfMonth: string;
+    month: string;
 };
 
 function Status({ location }: StatusProps) {
-   const [currentDate, setCurrentDate] = useState(new Date());
-   const [time, setTime] = useState<string | null>(null);
-   const [day, setDay] = useState<Day | null>(null);
-   const statusWrapperRef = useRef<HTMLDivElement>(null);
+    const [currentDate, setCurrentDate] = useState(new Date());
+    const [time, setTime] = useState<string | null>(null);
+    const [day, setDay] = useState<Day | null>(null);
+    const statusWrapperRef = useRef<HTMLDivElement>(null);
 
-   // 1s interval to update the time
-   useEffect(() => {
-      const intervalId = setInterval(() => {
-         setCurrentDate(new Date());
-      }, 1000); // Update every second
+    // 1s interval to update the time
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentDate(new Date());
+        }, 1000); // Update every second
 
-      return () => {
-         clearInterval(intervalId);
-      };
-   }, []);
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
 
-   // Animate the lines
-   useGSAP(() => {
-      if (!statusWrapperRef.current) return;
+    // Animate the lines
+    useGSAP(() => {
+        if (!statusWrapperRef.current) return;
 
-      gsap.fromTo(
-         ".gsap-line",
-         {
-            xPercent: -100,
-         },
-         {
-            xPercent: 0,
-            duration: 0.3,
-            ease: "expo4.inOut",
-            stagger: 0.25,
-         },
-      );
-   }, [statusWrapperRef.current]);
+        gsap.fromTo(
+            '.gsap-line',
+            {
+                xPercent: -100,
+            },
+            {
+                xPercent: 0,
+                duration: 0.3,
+                ease: 'expo4.inOut',
+                stagger: 0.25,
+            }
+        );
+    }, [statusWrapperRef.current]);
 
-   useEffect(() => {
-      const options = { timeZone: "Europe/London" };
+    useEffect(() => {
+        const options = { timeZone: 'Europe/London' };
 
-      const date = {
-         dayOfWeek: currentDate.toLocaleDateString("en-GB", {
-            weekday: "long",
-            ...options,
-         }),
-         dayOfMonth: currentDate.toLocaleDateString("en-GB", {
-            day: "numeric",
-            ...options,
-         }),
-         month: currentDate.toLocaleDateString("en-GB", {
-            month: "long",
-            ...options,
-         }),
-      };
+        const date = {
+            dayOfWeek: currentDate.toLocaleDateString('en-GB', {
+                weekday: 'long',
+                ...options,
+            }),
+            dayOfMonth: currentDate.toLocaleDateString('en-GB', {
+                day: 'numeric',
+                ...options,
+            }),
+            month: currentDate.toLocaleDateString('en-GB', {
+                month: 'long',
+                ...options,
+            }),
+        };
 
-      setDay(date);
-      setTime(
-         currentDate.toLocaleTimeString("en-GB", {
-            hour: "2-digit",
-            minute: "2-digit",
-            second: "2-digit",
-            timeZone: "Europe/London",
-         }),
-      );
-   }, [currentDate]);
+        setDay(date);
+        // Get user's timezone using Intl API
+        const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-   return (
-      time && (
-         <div
+        setTime(
+            currentDate.toLocaleTimeString('en-GB', {
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit',
+                timeZone: userTimeZone,
+            })
+        );
+    }, [currentDate]);
+
+    return (
+        <div
             ref={statusWrapperRef}
-            className={`flex flex-col w-1/2 *:text-body-medium lg:*:text-body-large overflow-clip uppercase ${statusWrapperRef.current ? "opacity-1" : "opacity-0"}`}
-         >
-            <h2 className="gsap-line">{location}</h2>
-            <span className="gsap-line">
-               {day?.dayOfWeek} | {day?.dayOfMonth} {day?.month}
+            className={`absolute right-0 flex w-1/2 flex-col overflow-clip uppercase *:text-body-medium lg:*:text-body-large`}
+        >
+            <h2 className='gsap-line'>{location}</h2>
+            <span className='gsap-line'>
+                {day?.dayOfWeek} | {day?.dayOfMonth} {day?.month}
             </span>
-            <span className="gsap-line">{time}</span>
-         </div>
-      )
-   );
+            <span className='gsap-line block'>{time}</span>
+        </div>
+    );
 }
 
 export default Status;
