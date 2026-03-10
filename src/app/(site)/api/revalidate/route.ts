ab0c5ab2164d4617950a1cf867c4130e1be03a05
juliscapucin <jscapucin@gmail.com>
2026-03-10 17:36:04 +0000

    add revalidate to contact page

--- src/app/(site)/api/revalidate/route.ts
+++ src/app/(site)/api/revalidate/route.ts
@@ -1,39 +1,44 @@
-import { NextRequest, NextResponse } from "next/server"
-import { revalidatePath } from "next/cache"
+import { NextRequest, NextResponse } from 'next/server';
+import { revalidatePath } from 'next/cache';
 
 export async function POST(req: NextRequest) {
-	const secret = req.nextUrl.searchParams.get("secret")
+    const secret = req.nextUrl.searchParams.get('secret');
 
-	if (secret !== process.env.SANITY_REVALIDATE_SECRET) {
-		console.error("❌ Invalid secret:", secret)
-		return NextResponse.json({ message: "Invalid token" }, { status: 401 })
-	}
+    if (secret !== process.env.SANITY_REVALIDATE_SECRET) {
+        console.error('❌ Invalid secret:', secret);
+        return NextResponse.json({ message: 'Invalid token' }, { status: 401 });
+    }
 
-	try {
-		const body = await req.json()
+    try {
+        const body = await req.json();
 
-		const slug = body?.slug?.current
-		const section = body?.section || body?.sectionSlug || body?._type // You can customize this logic
+        const slug = body?.slug?.current;
+        const section = body?.section || body?.sectionSlug || body?._type; // You can customize this logic
 
-		// Always revalidate the homepage
-		const pathsToRevalidate = ["/"]
+        // Always revalidate the homepage
+        const pathsToRevalidate = ['/', '/contact'];
 
-		// Revalidate /[section] if section is present
-		if (section) {
-			pathsToRevalidate.push(`/${section}`)
-		}
+        // Revalidate /[section] if section is present
+        if (section) {
+            pathsToRevalidate.push(`/${section}`);
+        }
 
-		// Revalidate /[section]/[slug] if both are present
-		if (section && slug) {
-			pathsToRevalidate.push(`/${section}/${slug}`)
-		}
+        // Revalidate /[section]/[slug] if both are present
+        if (section && slug) {
+            pathsToRevalidate.push(`/${section}/${slug}`);
+        }
 
-		await Promise.all(pathsToRevalidate.map((path) => revalidatePath(path)))
+        await Promise.all(
+            pathsToRevalidate.map((path) => revalidatePath(path))
+        );
 
-		console.log("✅ Revalidated:", pathsToRevalidate)
-		return NextResponse.json({ revalidated: true })
-	} catch (err) {
-		console.error("❌ Revalidation error:", err)
-		return NextResponse.json({ message: "Error revalidating" }, { status: 500 })
-	}
+        console.log('✅ Revalidated:', pathsToRevalidate);
+        return NextResponse.json({ revalidated: true });
+    } catch (err) {
+        console.error('❌ Revalidation error:', err);
+        return NextResponse.json(
+            { message: 'Error revalidating' },
+            { status: 500 }
+        );
+    }
 }
--- src/components/socialLinks.tsx
+++ src/components/socialLinks.tsx
@@ -1,35 +1,36 @@
-"use client";
+'use client';
 
-import Link from "next/link";
+import Link from 'next/link';
 
 type SocialsData = { platform: string; link: string };
 
 type SocialLinksProps = {
-   data: SocialsData[];
+    data: SocialsData[];
 };
 
 export default function SocialLinks({ data }: SocialLinksProps) {
-   return (
-      data && (
-         <div className="text-right mt-8 lg:mt-0 lg:self-end">
-            <h3 className="mb-4">Listen & Follow</h3>
-            {data.map((item) => {
-               return (
-                  <div
-                     className="relative flex justify-end items-start"
-                     key={item.platform}
-                  >
-                     <Link
-                        className="underlined-link block text-title-small md:text-title-medium lg:text-title-large uppercase"
-                        href={item.link}
-                        target="_blank"
-                     >
-                        {item.platform}
-                     </Link>
-                  </div>
-               );
-            })}
-         </div>
-      )
-   );
+    console.log(data);
+    return (
+        data && (
+            <div className='mt-8 text-right lg:mt-0 lg:self-end'>
+                <h3 className='mb-4'>Listen & Follow</h3>
+                {data.map((item) => {
+                    return (
+                        <div
+                            className='relative flex items-start justify-end'
+                            key={item.platform}
+                        >
+                            <Link
+                                className='underlined-link block text-title-small uppercase md:text-title-medium lg:text-title-large'
+                                href={item.link}
+                                target='_blank'
+                            >
+                                {item.platform}
+                            </Link>
+                        </div>
+                    );
+                })}
+            </div>
+        )
+    );
 }
